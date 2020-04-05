0e85574fa0101a912485663cb817d45d15f197f8
ghorsington <neigh@coder.horse>
2020-04-05 22:45:38 +0300

    Remove C++/CLI version

--- CM3D2.TexTool/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// CM3D2.TexTool.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
